dd31dbdc46b9b6c30197b11a9d003e7628285555
SCHOTTER Romain <47983209+romainschotter@users.noreply.github.com>
2024-08-28 00:02:39 +0200

    Rename mergeSameFileAsReal.sh to mergeFile.sh

--- mergeSameFileAsReal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INPUT_MC_REPO=alice
-declare -a RUN_LIST
-RUN_LIST=(526641 526964 527041 527057 527109 527240 527850 527871 527895 527899 528292 528461 528531)
-
-
-hadd AnalysisResults.root $(for key in "${!RUN_LIST[@]}"; do string=$(grep -r -l ${RUN_LIST[${key}]} ${INPUT_MC_REPO}) ; echo ${string/download_summary.txt/AnalysisResults.root}; done)
